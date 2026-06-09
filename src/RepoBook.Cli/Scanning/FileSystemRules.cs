11494b3e98bd6886911457f40f24f82873d3e0f0
Andrey <andrii@a2g.name>
2026-06-10 02:24:27 +0300

    added repo scanning logic and code detector

--- src/RepoBook.Cli/Scanning/FileSystemRules.cs
+++ src/RepoBook.Cli/Scanning/FileSystemRules.cs
@@ -2,23 +2,103 @@ namespace RepoBook.Scanning;
 
 public static class FileSystemRules
 {
+    private static readonly HashSet<string> IgnoredDirectories = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ".git",
+        ".hg",
+        ".svn",
+        "bin",
+        "obj",
+        "node_modules",
+        "bower_components",
+        "packages",
+        "vendor",
+        "dist",
+        "build",
+        "out",
+        "target",
+        "coverage",
+        ".next",
+        ".nuxt",
+        ".cache",
+        ".tmp",
+        "tmp",
+        ".idea",
+        ".vscode",
+        ".vs",
+        "__pycache__",
+        ".pytest_cache",
+        ".mypy_cache",
+        ".gradle",
+        "terraform.tfstate.d",
+    };
+
+    private static readonly HashSet<string> LockFiles = new(StringComparer.OrdinalIgnoreCase)
+    {
+        "package-lock.json",
+        "yarn.lock",
+        "pnpm-lock.yaml",
+        "Cargo.lock",
+        "poetry.lock",
+        "Pipfile.lock",
+        "composer.lock",
+    };
+
+    private static readonly HashSet<string> BinaryExtensions = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ".png",
+        ".jpg",
+        ".jpeg",
+        ".gif",
+        ".webp",
+        ".ico",
+        ".bmp",
+        ".tiff",
+        ".pdf",
+        ".zip",
+        ".gz",
+        ".tar",
+        ".7z",
+        ".rar",
+        ".exe",
+        ".dll",
+        ".so",
+        ".dylib",
+        ".bin",
+        ".mp3",
+        ".mp4",
+        ".mov",
+        ".avi",
+        ".mkv",
+        ".wav",
+        ".ttf",
+        ".otf",
+        ".woff",
+        ".woff2",
+        ".db",
+        ".sqlite",
+        ".sqlite3",
+        ".class",
+        ".jar",
+    };
+
     public static bool ShouldSkipDirectory(string directoryName)
     {
-        throw new NotImplementedException();
+        return IgnoredDirectories.Contains(directoryName);
     }
 
     public static bool ShouldSkipFile(string fileName)
     {
-        throw new NotImplementedException();
+        return string.Equals(fileName, "repository-encyclopedia.md", StringComparison.OrdinalIgnoreCase);
     }
 
     public static bool IsKnownBinaryExtension(string extension)
     {
-        throw new NotImplementedException();
+        return BinaryExtensions.Contains(extension);
     }
 
     public static bool IsLockFile(string fileName)
     {
-        throw new NotImplementedException();
+        return LockFiles.Contains(fileName);
     }
 }
--- src/RepoBook.Cli/Scanning/LanguageDetector.cs
+++ src/RepoBook.Cli/Scanning/LanguageDetector.cs
@@ -2,8 +2,87 @@ namespace RepoBook.Scanning;
 
 public sealed class LanguageDetector
 {
+    private static readonly Dictionary<string, string> ExtensionMappings = new(StringComparer.OrdinalIgnoreCase)
+    {
+        [".cs"] = "C#",
+        [".fs"] = "F#",
+        [".vb"] = "Visual Basic",
+        [".js"] = "JavaScript",
+        [".jsx"] = "JavaScript",
+        [".mjs"] = "JavaScript",
+        [".cjs"] = "JavaScript",
+        [".ts"] = "TypeScript",
+        [".tsx"] = "TypeScript",
+        [".py"] = "Python",
+        [".go"] = "Go",
+        [".rs"] = "Rust",
+        [".java"] = "Java",
+        [".kt"] = "Kotlin",
+        [".kts"] = "Kotlin",
+        [".swift"] = "Swift",
+        [".c"] = "C",
+        [".h"] = "C/C++ Header",
+        [".cpp"] = "C++",
+        [".cc"] = "C++",
+        [".cxx"] = "C++",
+        [".hpp"] = "C++ Header",
+        [".rb"] = "Ruby",
+        [".php"] = "PHP",
+        [".scala"] = "Scala",
+        [".sh"] = "Shell",
+        [".bash"] = "Shell",
+        [".zsh"] = "Shell",
+        [".ps1"] = "PowerShell",
+        [".sql"] = "SQL",
+        [".html"] = "HTML",
+        [".htm"] = "HTML",
+        [".css"] = "CSS",
+        [".scss"] = "SCSS",
+        [".sass"] = "Sass",
+        [".less"] = "Less",
+        [".json"] = "JSON",
+        [".yaml"] = "YAML",
+        [".yml"] = "YAML",
+        [".xml"] = "XML",
+        [".md"] = "Markdown",
+        [".txt"] = "Text",
+        [".dockerfile"] = "Dockerfile",
+        [".tf"] = "Terraform",
+        [".hcl"] = "HCL",
+        [".lua"] = "Lua",
+        [".r"] = "R",
+        [".dart"] = "Dart",
+        [".ex"] = "Elixir",
+        [".exs"] = "Elixir",
+        [".erl"] = "Erlang",
+        [".hrl"] = "Erlang",
+        [".clj"] = "Clojure",
+        [".cljs"] = "Clojure",
+    };
+
+    private static readonly Dictionary<string, string> FileNameMappings = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ["Dockerfile"] = "Dockerfile",
+        ["dockerfile"] = "Dockerfile",
+        ["Makefile"] = "Makefile",
+        ["CMakeLists.txt"] = "CMake",
+        ["Jenkinsfile"] = "Jenkins",
+    };
+
     public string DetectLanguage(string filePath)
     {
-        throw new NotImplementedException();
+        var fileName = Path.GetFileName(filePath);
+        if (FileNameMappings.TryGetValue(fileName, out var mappedFileNameLanguage))
+        {
+            return mappedFileNameLanguage;
+        }
+
+        var extension = Path.GetExtension(filePath);
+        if (ExtensionMappings.TryGetValue(extension, out var mappedExtensionLanguage))
+        {
+            return mappedExtensionLanguage;
+        }
+
+        return "Other";
     }
 }
